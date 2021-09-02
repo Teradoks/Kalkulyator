f19e54a7c135a3704f34f7f4a653e91ca442801f
Teradoks <tefgry@mail.ru>
2021-09-02 20:09:37 +0700

    Выбор математической операции

--- Calculator.js
+++ Calculator.js
@@ -12,5 +12,30 @@ let operation = null;
 function input_num1(){
   nl.question('Введите первое значение: ', function (argument) {
       num1 = parseFloat(argument);
+      select_opeation();
+  });
+}
+
+function select_opeation(){
+  nl.question('Выберите и введите номер нужной операции:\n\
+      1. Сложение\n\
+      2. Вычитание\n\
+      3. Умножение\n\
+      4. Деление\n\
+      5. Смена знака \n\
+      6. Выход\n\
+      : ',
+      function (argument) {
+      operation = parseInt(argument);
+      if (operation ==6){
+          nl.close();
+      }
+      else if(operation<6 || operation>0){
+
+      }
+      else{
+          console.log('Выбрана неверная операция');
+          nl.close();
+      }
   });
 }
