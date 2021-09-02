906d09e21604bb86d00a14c9f43f1634ba5bb449
Teradoks <tefgry@mail.ru>
2021-09-02 20:02:47 +0700

    Инициализация ввода и объявление переменных

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+const readline = require('readline');//подключение модуля readline
+
+const nl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let num1 = 0;
+let num2 = 0;
+let operation = null;
